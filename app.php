92793ce15bf36d61c1319bbc656dd30d30e9a9dc
jdreesen <jacob@hdreesen.de>
2011-07-16 21:51:28 +0200

    Added DoctrineExtension, implemented route "content" & added its view & database

--- app.php
+++ app.php
@@ -6,6 +6,15 @@ require_once __DIR__.'/vendor/silex/silex.phar';
 $app = new Silex\Application();
 
 // Add extensions
+$app->register(new Silex\Extension\DoctrineExtension(), array(
+    'db.options'            => array(
+        'driver'    => 'pdo_sqlite',
+        'path'      => __DIR__.'/app.sqlite',
+    ),
+    'db.dbal.class_path'    => __DIR__.'/vendor/doctrine-dbal/lib',
+    'db.common.class_path'  => __DIR__.'/vendor/doctrine-common/lib',
+));
+
 $app->register(new Silex\Extension\UrlGeneratorExtension());
 
 $app->register(new Silex\Extension\SymfonyBridgesExtension(), array(
@@ -26,10 +35,21 @@ $app->get('/', function () use ($app) {
 ->bind('homepage');
 
 $app->get('/{slug}/{page}', function ($slug, $page) use ($app) {
-    // correct page number as we are showing 2 pages every time
-    $page = ($page*2)-1;
+    $content = $app['db']->fetchAssoc("SELECT id, title FROM content WHERE slug = ?", array($slug));
+    $pages = $app['db']->fetchAll("SELECT page FROM page WHERE content_id = ? ORDER BY order_id ASC", array($content['id']));
     
-    return "$slug; $page";
+    return $app['twig']->render('content.html.twig', array(
+        'content' => array(
+            'title' => $content['title'],
+            'pages' => $pages
+        ),
+        'nav'     => array(
+            'previous' => $page-1,
+            'next'     => $page+1
+        ),
+        'slug'    => $slug,
+        'page'    => ($page*2)-1, // correct page number as we are showing 2 pages every time
+    ));
 })
 ->assert('page', '\d+')
 ->value('page', '1')
