998f00be6692299986b4efc14b226a51e70c9cf2
jdreesen <jacob@hdreesen.de>
2011-07-16 19:13:35 +0200

    added Twig (and some related) extensions and homepage view templates

--- app.php
+++ app.php
@@ -2,11 +2,28 @@
 
 require_once __DIR__.'/vendor/silex/silex.phar';
 
+// Create application
 $app = new Silex\Application();
 
-$app->get('/', function () {
-    return 'Herzlich willkommen!';
+// Add extensions
+$app->register(new Silex\Extension\UrlGeneratorExtension());
+
+$app->register(new Silex\Extension\SymfonyBridgesExtension(), array(
+    'symfony_bridges.class_path' => __DIR__.'/vendor',
+));
+
+$app->register(new Silex\Extension\TwigExtension(), array(
+    'twig.path'       => __DIR__.'/views',
+    'twig.class_path' => __DIR__.'/vendor/Twig/lib',
+));
+
+// Define routes
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html.twig', array(
+        'style' => rand(1, 4),
+    ));
 })
 ->bind('homepage');
 
+// Return application for reuse
 return $app;
